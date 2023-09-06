8073aa15e1522656d20c0d1c7dfca82265baf4c2
Emily Franco <143671702+xNovaPsyche@users.noreply.github.com>
2023-09-05 20:45:13 -0600

    Testing out and expanding the code from examples

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -1,9 +1,52 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
+using System;
 
-//Based on the HelloWorld File, turn this into a class.
+public class Program
+{
+    public Weapon goheiStaff;
+    public SpellCard weaponSpellCard;
+    
+    public void Main()
+    {
+        goheiStaff = new Weapon();
+        weaponSpellCard = new SpellCard();
 
-//Write a console program with a program class, and Main Function
-//Add another class with any name, add variables 
-//Using the new keyword create several objects
-//Console writeline the Objects variables
+        goheiStaff.bulletCount += 50;
+        goheiStaff.firePower += weaponSpellCard.powerLevel;
+        Console.WriteLine(goheiStaff.weaponName + " has a bullet count of " + goheiStaff.bulletCount + ".");
+        Console.WriteLine("Players magic enhance is now " + goheiStaff.firePower + "!");
+    }
+}
+public class Weapon {
+    public int bulletCount = 20;
+    public string weaponName = "Goddesses Blessing";
+    public float firePower = 5.99f;
+}
+public class SpellCard {
+    public float powerLevel = 33.5f;
+    public class Heroes
+{
+	public class Program
+    {
+    public Reimu shrineMaiden;
+	public Remilia mistressVampire;
+	
+	public void Main()
+	{
+		shrineMaiden = new Reimu();
+		mistressVampire = new Remilia();
+		
+		shrineMaiden.health = 25;
+		shrineMaiden.powerLevel = 50;
+		
+		mistressVampire.health = 100;
+		mistressVampire.powerLevel = 80;
+		
+		Console.WriteLine(shrineMaiden.health);
+		Console.WriteLine(shrineMaiden.powerLevel);
+		
+		Console.WriteLine(mistressVampire.health);
+		Console.WriteLine(mistressVampire.powerLevel);
+	}
+}
+}
+}
