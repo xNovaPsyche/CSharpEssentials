76af319b762c8b0cac10d1987f7eb8e68899980a
Emily Franco <143671702+xNovaPsyche@users.noreply.github.com>
2023-09-05 18:00:49 -0600

    Changed Hello to Hello How Are You for Practice

--- Basics/001 Conventions and Syntax/HelloWorld.cs
+++ Basics/001 Conventions and Syntax/HelloWorld.cs
@@ -8,6 +8,6 @@ public class HelloWorld
     {
         //Change "HW" to "Hello World"
         Console.WriteLine("HW");
-        Console.WriteLine("Hello");
+        Console.WriteLine("Hello How Are You");
     }    
 }
