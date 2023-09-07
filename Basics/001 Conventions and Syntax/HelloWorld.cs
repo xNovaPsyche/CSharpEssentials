1e415bb69b5d521088dbd2a40f71ba6ee4045434
Emily Franco <143671702+xNovaPsyche@users.noreply.github.com>
2023-09-07 16:44:57 -0600

    Changes made to HelloWorld.cs

--- Basics/001 Conventions and Syntax/HelloWorld.cs
+++ Basics/001 Conventions and Syntax/HelloWorld.cs
@@ -1,13 +1,10 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
 using System;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        //Change "HW" to "Hello World"
         Console.WriteLine("HW");
-        Console.WriteLine("Hello How Are You");
+        Console.WriteLine("Hello World");
     }    
 }
