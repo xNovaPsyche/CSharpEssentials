48f67b5d738cddea4d119994361ba89b9265c294
Emily Franco <143671702+xNovaPsyche@users.noreply.github.com>
2023-09-14 17:08:00 -0600

    Switching up variables and testing the compare system!

--- Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
+++ Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
@@ -2,6 +2,41 @@ using System;
                     
 public class Program
 {
-    public void Main() {}
+	public Operations myOperator;
+	
+    public void Main()
+    {
+        myOperator = new Operations();
+		
+		 Console.WriteLine("Temperature Adviser");
+        Console.WriteLine("Tempurature in Celcius:");
+		myOperator.DoMath(10, 0);
+		myOperator.DoMath(10, 10);
+		myOperator.DoMath(10, 20);
+		myOperator.Compare(4,3);
+		myOperator.Compare(3,4);
+		
+       
+		
+    }
+	
+	public class Operations {
+	public void DoMath (int value, int value2) {
+		var number = value + value2;
+		if (value < 30) 
+			
+			{
+  Console.WriteLine("It's a nice day outside!");
+}
+		Console.WriteLine(number);
+	} 
+	
+	public void Compare (int value, int value2) {
+		if(value < 30) {
+			Console.WriteLine("It's hot! Stay hydrated or hit the pool!");
+		}
+	}
     
+		
+	}
 }
