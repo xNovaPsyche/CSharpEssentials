dd2babe28dd0a03b775175376dd71496263d627d
Emily Franco <143671702+xNovaPsyche@users.noreply.github.com>
2023-09-14 16:23:51 -0600

    Getting Started

--- Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
+++ Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
@@ -1,4 +1,7 @@
-// The Expectation is that you write your own code based
-// on what you have learned in the class, it can be what you want
-// If you are havind trouble thinging of something, write conditionals
-// with the topic of PowerUps, what happens if the powerUp is good or bad.
+using System;
+                    
+public class Program
+{
+    public void Main() {}
+    
+}
